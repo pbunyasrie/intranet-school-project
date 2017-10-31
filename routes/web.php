cc43ce778b93de01970b7177aac38585dcab8a56
Prasit Bunyasrie <pbunyasrie@vcu.edu>
2017-10-31 15:43:15 -0400

    Remove some testing/boilerplate stuff

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,7 @@
 |
 */
 
-Route::get('/test', function () {
 
-	$client = \Vaites\ApacheTika\Client::make('/usr/local/Cellar/tika/1.16/libexec/tika-app-1.16.jar');
-	$text = $client->getText('/tmp/test.pdf');
-	$metadata = $client->getMetadata('/tmp/test.pdf');
-
-    return view('welcome', compact('text', 'metadata'));
-});
 Route::get('/SearchQuery', 'HomeController@search');
 
 Auth::routes();
