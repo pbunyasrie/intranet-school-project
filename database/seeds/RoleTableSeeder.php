b74952c69c7eb00d781ff92afa9fdd614bf86a82
Prasit Bunyasrie <pbunyasrie@vcu.edu>
2017-10-31 15:33:30 -0400

    Update roles

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -8,17 +8,23 @@ class RoleTableSeeder extends Seeder
   {
     $role_employee = new Role();
     $role_employee->name = 'user';
-    $role_employee->description = 'A regular user';
+    $role_employee->description = 'Hospital users (faculty, interns, groups, etc.)';
+    /*
+        Manage (create, edit, delete) folders for content (a folder would correspond to a specific project or group).
+
+        Allowed to search for and view content areas that they are authorized to view
+        If so authorized, they can upload, edit, and delete content
+    */
     $role_employee->save();
 
     $role_manager = new Role();
-    $role_manager->name = 'manager';
-    $role_manager->description = 'A manager';
+    $role_manager->name = 'surveyor';
+    $role_manager->description = 'A surveyor with limited access'; // Login and search for and view selected content determined by the Site Manager
     $role_manager->save();
 
     $role_manager = new Role();
-    $role_manager->name = 'admin';
-    $role_manager->description = 'An admin';
+    $role_manager->name = 'Site Manager';
+    $role_manager->description = 'Creates user accounts and manage folders';
     $role_manager->save();
   }
 }
