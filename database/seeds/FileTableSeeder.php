f811562453e2626712233a51ba7fc6dda9cd1c48
Prasit Bunyasrie <pbunyasrie@vcu.edu>
2017-11-06 19:35:06 -0500

    Fix database seeding

--- database/seeds/FileTableSeeder.php
+++ database/seeds/FileTableSeeder.php
@@ -12,18 +12,6 @@ class FileTableSeeder extends Seeder
      */
     public function run()
     {
-	    $file = new File();
-	    $file->folder_id = 1;
-	    $file->filename = 'not a real file.pdf';
-	    $file->filepath = 'somewhere/asdf34.pdf';
-	    $file->contents = 'Example contents';
-	    $file->save();
-
-	    $file = new File();
-	    $file->folder_id = 2;
-	    $file->filename = 'not a real file 2.pdf';
-	    $file->filepath = 'somewhere/asdgast3.pdf';
-	    $file->contents = 'Example contents';
-	    $file->save();
+    	
     }
 }
