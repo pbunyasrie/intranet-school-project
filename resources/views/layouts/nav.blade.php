a2792182cf2e03ab2cf2847049f2cdf90ac9e4b0
Prasit Bunyasrie <pbunyasrie@vcu.edu>
2017-11-23 18:15:30 -0500

    Add logo/image to header

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,11 +2,13 @@
 <nav class="navbar is-white">
   <div class="container">
     <div class="navbar-brand">
-      <h1 class="is-size-3">
-          <a class="navbar-item brand-text" href="/">
-            {{ config('app.name', 'Intranet') }}          
-          </a>
-      </h1>
+    	<figure>
+	      <a style="color: #000" href="/">
+    		<img src="/images/logo.png" style="max-width: 50%; max-height: 50%; margin-top: 10px" />
+    		<br />
+	        {{ config('app.name', 'Intranet') }}          
+	      </a>
+    	</figure>
     </div>
     
   </div>
